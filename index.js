cdeb1f3d0dc85b819c1508c8e54f57f3e85d6b1c
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-07 12:52:23 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+// Write your solution here
+let cats = ['Milo', 'Otis', 'Garfield' ]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    let undefined = cats.slice()
+    undefined.push(name)
+    return undefined
+}
+function prependCat(name){
+    let undefined = cats.slice()
+    undefined.unshift(name)
+    return undefined
+}
+
+function removeLastCat(){
+    let undefined = cats.slice()
+    undefined.pop()
+    return undefined
+
+}
+function removeFirstCat(){
+    let undefined = cats.slice()
+    undefined.shift()
+    return undefined
+}
